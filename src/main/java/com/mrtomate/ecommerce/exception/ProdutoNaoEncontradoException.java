448065c3092e317a58b3ca3578f4753e6c1cf8d8
MrTomatePNG <mrtomate360@outlook.com>
2026-01-22 17:38:41 -0400

    correção de visibilidade

--- src/main/java/com/mrtomate/ecommerce/exception/ProdutoNaoEncontradoException.java
+++ src/main/java/com/mrtomate/ecommerce/exception/ProdutoNaoEncontradoException.java
@@ -2,7 +2,7 @@ package com.mrtomate.ecommerce.exception;
 
 public class ProdutoNaoEncontradoException extends RuntimeException {
 
-    ProdutoNaoEncontradoException(String mensagem) {
+    public ProdutoNaoEncontradoException(String mensagem) {
         super(mensagem);
     }
 }
