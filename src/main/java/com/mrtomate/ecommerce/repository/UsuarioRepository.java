793edd8091c337c1a40ade4974fbc0e3927e08f2
MrTomatePNG <mrtomate360@outlook.com>
2026-01-22 22:20:26 -0400

    Create UsuarioRepository.java

---
+++ src/main/java/com/mrtomate/ecommerce/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.mrtomate.ecommerce.repository;
+
+import com.mrtomate.ecommerce.models.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
